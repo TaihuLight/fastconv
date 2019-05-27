7f9e08e3429b79e0d4a21693339c253f2457a05e
WeiHua Lee <lwh1987@mail.ustc.edu.cn>
2019-05-27 22:00:36 +0800

    add comments

--- README.md
+++ README.md
@@ -15,4 +15,4 @@ TODO:
   - [ ] other format winograd convolution, such as F(4x4, 3x3), F(6x6, 3x3), sparse winograd convolution
 
 ## fft convolution
-[fft 1d convolution example](fft_conv1d_example.py)
+fft 1d convolution [example](fft_conv1d_example.py)
--- fft_conv1d_example.py
+++ fft_conv1d_example.py
@@ -3,13 +3,28 @@ from numpy.fft import fft
 from numpy.fft import ifft
 from scipy.signal import convolve
 
-# linear convolution, padding with p = k-1
+"""
+signal and filter are both 1d
+signal length is L
+filter length is M
+result of linear/full convolution length is L+M-1
+result of circular/valid convolution length is L-M+1
+result of same convolution length is L-M+1
+In general, when stride = 1, the output size is L-M+2p+1
+"""
+
+# linear convolution, padding with p = M-1
 def LinearConv(x, k):
     return convolve(x, k, 'full')
 
 # valid convolution, also no padding
 def CircularConv(x, k):
     return convolve(x, k, 'valid')
+
+# save convolution, padding with p = (M-1)/2
+def SameConv(x, k):
+    return convolve(x, k, 'same')
+
 
 # We need zero padding the signal filter ending,
 # and the length must be at least L + M -1.
@@ -34,9 +49,35 @@ def FFTCircularConv(x, k, extpad = 0):
 
     L = x.shape[0]
     M = k.shape[0]
-    T = L + extpad
     T0 = L - M + 1
+    T = L + extpad
     return np.real(ifft(fft(x, T) * fft(k, T)))[M-1: M-1+T0]
+
+def FFTCircularConv2(x, k, extpad = 0):
+    assert len(x.shape) == 1
+    assert len(k.shape) == 1
+    assert extpad >= 0
+
+    L = x.shape[0]
+    M = k.shape[0]
+    T0 = L - M + 1
+    T = L + M - 1 + extpad
+    return np.real(ifft(fft(x, T) * fft(k, T)))[M-1: M-1+T0]
+
+
+# We need zero padding the signal filter ending,
+# and the length must be at least L + M -1.
+# extpad means extra padding at the end of signal and filter
+def FFTSameConv(x, k, extpad = 0):
+    assert len(x.shape) == 1
+    assert len(k.shape) == 1
+    assert extpad >= 0
+
+    L = x.shape[0]
+    M = k.shape[0]
+    T0 = L 
+    T = L + M - 1 + extpad
+    return np.real(ifft(fft(x, T) * fft(k, T)))[(M-1)//2: (M-1)//2 + T0]
 
 
 # check the result
@@ -50,13 +91,15 @@ def CheckResult(x, y, tol = 1e-4):
 
 if __name__ == "__main__":
     L = 10
-    M = 5
+    M = 3
     x = np.random.random(L)
     k = np.random.random(M)
     conv1 = LinearConv(x, k)
     conv2 = FFTLinearConv(x, k, 3)
     conv3 = CircularConv(x, k)
-    conv4 = FFTCircularConv(x, k, 4)
+    conv4 = FFTCircularConv2(x, k, 3)
+    conv5 = SameConv(x, k)
+    conv6 = FFTSameConv(x, k, 3)
 
     print("signal:\n", x)
     print("filter:\n", k)
@@ -64,6 +107,8 @@ if __name__ == "__main__":
     print("FFTLinearConv:\n", conv2)
     print("CircularConv:\n", conv3)
     print("FFTCircularConv:\n", conv4)
+    print("SameConv:\n", conv5)
+    print("FFTSameConv:\n", conv6)
 
     if CheckResult(conv1, conv2):
         print("Check LinearConv success.")
@@ -74,4 +119,9 @@ if __name__ == "__main__":
         print("Check CircularConv success.")
     else:
         print("Check FFTCircularConv fail!")
+
+    if CheckResult(conv5, conv6):
+        print("Check SameConv success.")
+    else:
+        print("Check FFTSameConv fail!")
 
